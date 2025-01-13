50e2d56f9c2564c7693e9ec3230505fca890a11b
ffavre13 <192215936+ffavre13@users.noreply.github.com>
2025-01-13 13:27:51 +0100

    Add audio class

--- src/components/DialogBox.scala
+++ src/components/DialogBox.scala
@@ -1,22 +1,23 @@
 package components
 
-import javax.swing.{JFrame, JOptionPane}
+import javax.swing.{JButton, JFrame, JOptionPane}
 
 object DialogBox {
   /**
    * Shows a dialog using JOptionPane object
-   * @param title Title of the Dialog
+   *
+   * @param title   Title of the Dialog
    * @param content Content of the Dialog
    */
   def showDialog(title: String, content: String): Unit = {
-    val frame = new JFrame(title)
+    val frame: JFrame = new JFrame(title)
     JOptionPane.showMessageDialog(frame, content)
   }
 
   def showDialogReplay(title: String, content: String): Boolean = {
-    val frame = new JFrame(title)
+    val frame: JFrame = new JFrame(title)
 
-    val choice: Int = JOptionPane.showOptionDialog(frame, content, title, JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE, null, null, JOptionPane.YES_OPTION)
+    val choice: Int = JOptionPane.showOptionDialog(frame, content, title, JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null,null, JOptionPane.YES_OPTION)
 
     choice == JOptionPane.YES_OPTION
   }
